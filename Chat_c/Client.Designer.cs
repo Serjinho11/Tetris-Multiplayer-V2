de7ca3e995204f7823e8c2be169c7137788d780d
Serjinho11 <44323117+Serjinho11@users.noreply.github.com>
2020-12-23 21:54:23 +0200

    Add files via upload

---
+++ Chat_c/Client.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Chat_c
+{
+    partial class fClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.pbBackGround = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConect
+            // 
+            this.btnConect.Location = new System.Drawing.Point(614, 59);
+            this.btnConect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConect.Name = "btnConect";
+            this.btnConect.Size = new System.Drawing.Size(125, 28);
+            this.btnConect.TabIndex = 3;
+            this.btnConect.Text = "Connect";
+            this.btnConect.UseVisualStyleBackColor = true;
+            this.btnConect.Click += new System.EventHandler(this.btnConect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(62, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(62, 409);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(62, 497);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(62, 595);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // pbBackGround
+            // 
+            this.pbBackGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbBackGround.Image = global::Chat_c.Properties.Resources.ClientBackground;
+            this.pbBackGround.Location = new System.Drawing.Point(0, 0);
+            this.pbBackGround.Name = "pbBackGround";
+            this.pbBackGround.Size = new System.Drawing.Size(1034, 645);
+            this.pbBackGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBackGround.TabIndex = 8;
+            this.pbBackGround.TabStop = false;
+            // 
+            // fClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 645);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnConect);
+            this.Controls.Add(this.pbBackGround);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fClient";
+            this.Text = "Chat-Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackGround)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pbBackGround;
+    }
+}
+
---
+++ Chat_c/Client.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_c
+{
+    public partial class fClient : Form
+    {
+        public TcpClient client;
+        public NetworkStream clientStream;
+        public bool ascult;
+        public fClient clientForm;
+        public Thread t;
+
+        public fClient()
+        {
+            InitializeComponent();
+            clientForm = this;
+
+        }
+
+        private void Asculta_client()
+        {
+            StreamReader citire = new StreamReader(clientStream);
+            String dateClient;
+            while (ascult)
+            {
+                dateClient = citire.ReadLine();
+                //MethodInvoker m = new MethodInvoker(() => clientForm.textBox1.Text += ("Server: " + dateClient + Environment.NewLine));
+                //clientForm.textBox1.Invoke(m);
+            }
+        }
+        /*
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                //scriere.WriteLine(tbDate.Text);
+               // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+               // tbDate.Clear();
+               // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+              //  client.Close();
+            } 
+        }
+        */
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ascult = false;
+            t.Abort();
+            clientStream.Close();
+            client.Close();
+        }
+        /*
+        private void tbDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnSend_Click(sender, e);
+            }
+        }
+        */
+        private void btnConect_Click(object sender, EventArgs e)
+        {
+            if (btnConect.Text == "Connect")
+            {
+                //if (tbAddress.Text.Length > 0)
+                // {
+                client = new TcpClient("127.0.0.1", 3000);
+                ascult = true;
+                t = new Thread(new ThreadStart(Asculta_client));
+                t.Start();
+                clientStream = client.GetStream();
+
+                // tbDate.Enabled = true;
+                // btnSend.Enabled = true;
+                // label1.Visible = false;
+                // tbAddress.Visible = false;
+                btnConect.Text = "Disconect";
+                //}
+                // else
+                // {
+                //     MessageBox.Show("Specificati adresa de IP");
+                //  }
+            }
+            else
+            {
+                ascult = false;
+                t.Abort();
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine("#Gata");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 1 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 2 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 3 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 4 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 5 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 6 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(clientStream);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(" 7 ");
+                // textBox1.Text += "Client: " + tbDate.Text + Environment.NewLine;
+                // tbDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            }
+        }
+    }
+}
